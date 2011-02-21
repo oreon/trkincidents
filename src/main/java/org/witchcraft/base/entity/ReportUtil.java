533e1fdfa2675c7c45492f86a2847e598932d5c8
singhgurpreet <singhgurpreet@38423737-2f20-0410-893e-9c0ab9ae497d>
2011-02-21 20:07:47 +0000

    Added for reports

---
+++ src/main/java/org/witchcraft/base/entity/ReportUtil.java
@@ -0,0 +1,12 @@
+package org.witchcraft.base.entity;
+
+
+public class ReportUtil {
+	
+	public static String getMonth(int month){
+		
+		return new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}[month];
+		
+	}
+
+}
